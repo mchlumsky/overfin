e5396e6626b17beaca6b8facb6f4a4f71346e5ad
Martin Chlumsky <martin.chlumsky@gmail.com>
2025-10-26 08:37:14 -0400

    fix: add missing terra repo and drop zed

--- build_files/build.sh
+++ build_files/build.sh
@@ -15,7 +15,9 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-dnf5 -y install --enable-repo terra ghostty ripgrep neovim headsetcontrol golang gdu btop gnome-shell-extension-no-overview shotwell pcp zed
+dnf -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
+
+dnf5 -y install --enable-repo terra ghostty ripgrep neovim headsetcontrol golang gdu btop gnome-shell-extension-no-overview shotwell pcp
 
 dnf5 -y copr enable dwmw2/openconnect
 dnf5 -y update NetworkManager-openconnect openconnect
